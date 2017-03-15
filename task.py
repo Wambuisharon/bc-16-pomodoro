b4111c0605875e77438a9571cc2fa0c8d74935b2
Wambui Sharon <wambuisha50@gmail.com>
2017-03-15 21:42:35 +0300

    Add verbosity

--- task.py
+++ task.py
@@ -15,6 +15,23 @@ class Task():
 		self.long_break =long_break
 		self.duration = duration
 
+	def go_long_break(self):
+		counter = self.long_break
+		while counter > 0:
+			sys.stdout.write('\rLONG BREAK:' + seconds_to_time(self.time_left))
+	    	sys.stdout.flush()
+			time.sleep(1)
+			counter -=1	
+
+
+	def go_short_break(self):
+		counter = self.short_break
+		while counter > 0:
+			sys.stdout.write('\rSHORT BREAK:' + seconds_to_time(self.time_left))
+	    	sys.stdout.flush()
+			time.sleep(1)
+			counter -=1	
+
 	def start(self):
 		self.status = 'running'
 		self.time_left = self.duration
@@ -24,13 +41,21 @@ class Task():
 	   		time.sleep(1)
 	    	self.time_left -=1
 	    	counter += 1
+	    	sys.stdout.write('\rTIME LEFT:' + seconds_to_time(self.time_left))
+	    	sys.stdout.flush()
 	    	if (counter == self.cycle_time):
 		 		self.current_cycle +=1
 		 		if(self.current_cycle%4==0):
 		 			#go long break
-		 			pass
+		 			self.go_long_break()
+		 			
 		 		else:
 		 			#go short break
-		 			pass
+		 			self.go_short_break()
+		 		
 		 			#reset break counter
 		 		counter = 0	
+def seconds_to_time(seconds):
+	m, s =divmod(seconds, 60)	
+	h, s =divmod(m, 60)	
+	return "%d:%02d:%02d" %(h, m, s)	 	
