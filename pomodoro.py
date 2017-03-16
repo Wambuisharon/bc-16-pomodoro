91cc9c606f74ad319b7f440c80b9edf189fac057
Wambui Sharon <wambuisha50@gmail.com>
2017-03-16 21:38:58 +0300

    Add stop function

--- pomodoro.py
+++ pomodoro.py
@@ -1,10 +1,27 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import signal, sys, time
 
 from task import Task
-from databases import Storage
+from database import Storage
 from task import seconds_to_time
 
-task = 0
-print ("Pomodoro intro here!")
+task = None
+print("Pomodoro intro here!")
+
+# signal number and current app frame for the keyboard interrupts
+def stop_timer(signum, frame):
+    if task is not None and task.status is not 'stopped':
+        print('Stopping current task:' + task.name + '')
+        task.status = "stopped"
+        time.sleep(0.5)
+        sys.stdout.write('\r                                    ')
+        sys.stdout.flush()
+    else:
+        sys.exit(0)
+
+
+signal.signal(signal.SIGINT, stop_timer)
 
 while (True):
     comand = raw_input("pomodoro: ")
@@ -14,7 +31,7 @@ while (True):
         task.status = 'pending'
     elif comands[0] == 'config':
         if task is None:
-            print ("Start a task first")
+            print("Start a task first")
         else:
             if comands[1] == 'time':
                 task.duration = 60 * int(comands[2])
@@ -27,10 +44,10 @@ while (True):
             elif comands[1] == 'sound':
                 task.alarm = bool(comands[2])
             else:
-                print ("Invalid command")
+                print("Invalid command")
     elif comands[0] == 'run':
         if task is None:
-            print ("Start a task first")
+            print("Start a task first")
         else:
             task.status = 'running'
             db = Storage()
@@ -40,6 +57,6 @@ while (True):
     elif comands[0] == 'list':
         db = Storage()
         for task in db.get():
-            print ("Name: %s, Duration:  %s,  Cycle Time: %s, Short Break %s, Long Break %s, Alarm %s, Status %s" % (
-            task.name, seconds_to_time(task.duration), seconds_to_time(task.cycle_time),
-            seconds_to_time(task.short_break), seconds_to_time(task.long_break), str(task.alarm), task.status))
+            print("Name: %s, Duration:  %s,  Cycle Time: %s, Short Break %s, Long Break %s, Alarm %s, Status %s" % (
+                task.name, seconds_to_time(task.duration), seconds_to_time(task.cycle_time),
+                seconds_to_time(task.short_break), seconds_to_time(task.long_break), str(task.alarm), task.status))
